00c654c39bc1649177d5dc7e40afad758c871814
Jessica Mo <jessica.mo@rea-group.com>
2022-12-09 23:17:14 +1100

    Robot simulator code structure

--- src/main/java/com/robot/game/Robot.java
+++ src/main/java/com/robot/game/Robot.java
@@ -1,5 +1,27 @@
 package com.robot.game;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Robot {
     //todo pass movement to position
+    private Position position;
+
+    public void move() {
+
+    }
+    public void turnLeft() {
+
+    }
+    public void turnRight() {
+
+    }
+    public void placeRobot(Position initialPosition, MapTable maptable) {
+
+    }
+    public String reportPosition() {
+        return position.toString();
+    }
 }
--- src/main/java/com/robot/game/Simulator.java
+++ src/main/java/com/robot/game/Simulator.java
@@ -1,5 +1,22 @@
 package com.robot.game;
 
+import com.robot.utility.Command;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Simulator {
     // pass command to Robot
+    private MapTable mapTable;
+    private Robot robot;
+    public String executeCommand(String commandLine){
+       return null;
+    }
+    private void placeCommand(String commands){
+
+    }
+    private String otherCommand(Command currentCommand){
+        return null;
+    }
 }
