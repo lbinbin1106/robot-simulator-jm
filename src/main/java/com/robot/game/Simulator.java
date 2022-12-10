eb5236207d91e2bd12736ec9fe02904c7796480b
Jessica Mo <jessica.mo@rea-group.com>
2022-12-10 22:57:06 +1100

    Command extraction

--- src/main/java/com/robot/game/Simulator.java
+++ src/main/java/com/robot/game/Simulator.java
@@ -1,5 +1,6 @@
 package com.robot.game;
 
+import com.robot.exception.InvalidGameRobotException;
 import com.robot.utility.Command;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,9 +12,31 @@ public class Simulator {
     private MapTable mapTable;
     private Robot robot;
     public String executeCommand(String commandLine){
-       return null;
+        String[] commandArrays = commandLine.split(" ");
+        String output = "";
+        try {
+            Command command = Command.valueOf(commandArrays[0]);
+            if (command == Command.PLACE) {
+                placeCommand(commandArrays[1]);
+            } else {
+                otherCommand(command);
+            }
+        } catch (IllegalArgumentException ex) {
+            throw new InvalidGameRobotException("Please enter valid command");
+        }
+        return output;
     }
     private void placeCommand(String commands){
+        int xValue = 0;
+        int yValue = 0;
+        String[] commandPositionArrays = commands.split(",");
+        try {
+            xValue = Integer.valueOf(commandPositionArrays[0]);
+            yValue = Integer.valueOf(commandPositionArrays[1]);
+        } catch (Exception ex) {
+            throw new InvalidGameRobotException("Please only enter numbers");
+        }
+        //Direction and position assigning
 
     }
     private String otherCommand(Command currentCommand){
