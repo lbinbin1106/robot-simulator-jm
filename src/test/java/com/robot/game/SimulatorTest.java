ab23dde43ca458f7db54e8e4d709b1cea0341c6d
Jessica Mo <jessica.mo@192-168-1-111.tpgi.com.au>
2023-01-07 23:09:46 +1100

    test simlator

--- src/test/java/com/robot/game/SimulatorTest.java
+++ src/test/java/com/robot/game/SimulatorTest.java
@@ -1,4 +1,14 @@
 package com.robot.game;
 
 public class SimulatorTest {
+    // test positive place command
+    // test invalid place command (ex plac)
+    // test invalid place command when position is null
+    // test invalid command position (not numbers)
+    // test invalid command position (nunnbers > 5)
+
+    // test positive other commands (left, right, move and report)
+    // test other command is null
+    // test invalid other command when position is null
+
 }
